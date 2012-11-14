9c2bf4e228012e0a2c7bc3d551e76a0a20e3fe94
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-11-14 12:52:13 -0600

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Rackspace US, Inc."
 license           "Apache 2.0"
 description       "Installs and configures the Keystone Identity Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.12"
+version           "1.0.13"
 recipe            "keystone::server", "Installs packages required for a keystone server"
 
 %w{ ubuntu fedora }.each do |os|
