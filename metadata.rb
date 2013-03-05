638463700d7ab1b49fe6115a4b0589c44c4b3cc1
rcbjenkins <rcb-deploy@lists.rackspace.com>
2013-03-05 15:06:28 -0600

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Rackspace US, Inc."
 license           "Apache 2.0"
 description       "Installs and configures the Keystone Identity Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.17"
+version           "1.0.18"
 recipe            "keystone::server", "Installs packages required for a keystone server"
 recipe            "keystone::keystone-api", "Installs packages required for an additional keystone server in HA environment"
 
