43c825eff6d690d5ba15b996eb203bbd15a3e7b5
rackerjoe <breu@breu.org>
2013-09-09 16:21:30 -0500

    Updating version # to 4.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Rackspace US, Inc."
 license           "Apache 2.0"
 description       "Installs and configures the Keystone Identity Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.20"
+version           "4.1.2"
 recipe            "keystone::setup", "Installs packages required for a keystone server"
 recipe            "keystone::keystone-api", "Installs packages required for an additional keystone server in HA environment"
 recipe            "keystone::keystone-ssl", "Sets up Keystone to use Apache mod_wsgi"
