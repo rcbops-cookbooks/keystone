a90bc6a82421dcc8d078036532bede1e52e50d0b
rackerjoe <breu@breu.org>
2013-10-11 09:32:33 -0500

    move version out of metadata.rb into VERSION

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Rackspace US, Inc."
 license           "Apache 2.0"
 description       "Installs and configures the Keystone Identity Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "4.1.3"
+version           IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 recipe            "keystone::setup", "Installs packages required for a keystone server"
 recipe            "keystone::keystone-api", "Installs packages required for an additional keystone server in HA environment"
 recipe            "keystone::keystone-ssl", "Sets up Keystone to use Apache mod_wsgi"
